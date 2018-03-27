32cf0f7463d59ccfc574e9a292bdcb4be7c95c1b
吴技宸 <wujichen@wujichendeMacBook-Air.local>
2018-03-27 01:29:20 -0400

    DFS III

---
+++ DFS/695. Max Area of Island.cpp
@@ -0,0 +1,124 @@
+/*
+695. Max Area of Island
+
+Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+
+Example 1:
+[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Given the above grid, return 6. Note the answer is not 11, because the island must be connected 4-directionally.
+Example 2:
+[[0,0,0,0,0,0,0,0]]
+Given the above grid, return 0.
+Note: The length of each dimension in the given grid does not exceed 50.
+
+
+
+*/
+
+
+class Solution {
+public:
+    int n, m;
+    vector<vector<bool>> visited;
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        if(grid.size()==0) return 0;
+        int result = 0;
+        n = grid.size(); m = grid[0].size();
+        visited.resize(n,vector<bool>(m,false));
+        for(int i = 0; i<n ; i++)
+            for(int j = 0; j<m; j++)
+                if(!visited[i][j] && grid[i][j] == 1)
+                    result = max(result, dfs(grid,i,j));
+        return result;        
+    }
+    
+    int dfs(vector<vector<int>>& grid, int i, int j){
+        if(i<0 || j<0  || i>= n || j>=m || grid[i][j]!= 1 || visited[i][j]) return 0;
+        visited[i][j] = true;
+        int cumsum = 1;
+        cumsum += dfs(grid,i-1, j);
+        cumsum += dfs(grid,i, j-1);
+        cumsum += dfs(grid,i+1, j);
+        cumsum += dfs(grid,i, j+1);
+        return cumsum;
+    }
+    
+};
+
+
+class Solution {
+public:
+    int n, m;
+    vector<vector<bool>> visited;
+    vector<vector<int>>dir;
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        if(grid.size()==0) return 0;
+        int result = 0;
+        n = grid.size(); m = grid[0].size();
+        dir = {{-1,0},{0,-1},{1,0},{0,1}};
+        visited.resize(n,vector<bool>(m,false));
+        for(int i = 0; i<n ; i++)
+            for(int j = 0; j<m; j++)
+                if(!visited[i][j] && grid[i][j] == 1)
+                    result = max(result, bfs(grid,i,j));
+        return result;        
+    }
+    
+    int bfs(vector<vector<int>>& grid, int i, int j){
+        visited[i][j] = true;
+        int cumsum = 0;
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        while(q.size()){
+            cumsum++;
+            i = q.front().first;
+            j = q.front().second;
+            q.pop();
+            for(int k = 0; k<4; k++){
+                int x = i+dir[k][0];
+                int y = j+dir[k][1];
+                if(x<0 || y<0  || x>= n || y>=m || grid[x][y]!= 1 || visited[x][y]) continue;
+                visited[x][y] = true;
+                q.push({x,y});
+            }
+            
+        }
+        return cumsum;
+    }
+    
+};
+
+
+
+class Solution {
+public:
+    int n, m;
+    vector<vector<bool>> visited;
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        if(grid.size()==0) return 0;
+        int result = 0;
+        n = grid.size(); m = grid[0].size();
+        visited.resize(n,vector<bool>(m,false));
+        for(int i = 0; i<n ; i++)
+            for(int j = 0; j<m; j++)
+                if(!visited[i][j] && grid[i][j] == 1)
+                    result = max(result, dfs(grid,i,j));
+        return result;        
+    }
+    
+    int dfs(vector<vector<int>>& grid, int i, int j){
+        if(i<0 || j<0  || i>= n || j>=m || grid[i][j]!= 1 || visited[i][j]) return 0;
+        visited[i][j] = true;
+        return dfs(grid,i-1, j) + dfs(grid,i, j-1) + dfs(grid,i+1, j) + dfs(grid,i, j+1) + 1;
+    }
+    
+};
---
+++ DFS/733. Flood Fill.cpp
@@ -0,0 +1,56 @@
+/*
+
+733. Flood Fill
+
+Time	    Space	Difficulty
+O(m * n)	O(m * n)	Easy
+不需要visited unorderdmap 来记录到过的点
+
+
+An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
+
+Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+
+To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+
+At the end, return the modified image.
+
+Example 1:
+Input: 
+image = [[1,1,1],[1,1,0],[1,0,1]]
+sr = 1, sc = 1, newColor = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: 
+From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected 
+by a path of the same color as the starting pixel are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected
+to the starting pixel.
+Note:
+
+The length of image and image[0] will be in the range [1, 50].
+The given starting pixel will satisfy 0 <= sr < image.length and 0 <= sc < image[0].length.
+The value of each color in image[i][j] and newColor will be an integer in [0, 65535].
+
+
+*/
+
+class Solution {
+public:
+    int n, m;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        n = image.size(); m = image[0].size();
+        if(image[sr][sc]!=newColor)
+            dfs(image,sr,sc,newColor, image[sr][sc]);
+        return image;
+    }
+    
+    void dfs(vector<vector<int>>& image, int sr, int sc, const int & newColor,int startColor) {
+        //cout<<"sr "<<sr<<" sc "<<sc<<" startcolor "<<startColor<<endl;
+        if(sr<0 || sc<0 || sr>= n || sc>=m || image[sr][sc]!=startColor ) return;
+        image[sr][sc] = newColor;
+        dfs(image,sr-1,sc,newColor,startColor);
+        dfs(image,sr,sc-1,newColor,startColor);
+        dfs(image,sr+1,sc,newColor,startColor);
+        dfs(image,sr,sc+1,newColor,startColor);
+    }
+};
