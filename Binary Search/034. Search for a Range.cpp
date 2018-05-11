996d6ef4ee9f9b562a2e832d1b1a45fcac54aa49
beckswu <beckswu@gmail.com>
2018-05-11 15:39:49 -0400

    Update 034. Search for a Range.cpp

--- Binary Search/034. Search for a Range.cpp
+++ Binary Search/034. Search for a Range.cpp
@@ -169,3 +169,11 @@ private:
         return left;
     }
 };
+
+
+vector<int> searchRange(vector<int>& nums, int target) {
+    auto bounds = equal_range(nums.begin(), nums.end(), target);
+    if (bounds.first == bounds.second)
+        return {-1, -1};
+    return {bounds.first - nums.begin(), bounds.second - nums.begin() - 1};
+}
