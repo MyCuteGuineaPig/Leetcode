f6035471ce69e89a9e2672e745b58a1e000f59af
吴技宸 <wujichen@wujichendeMacBook-Air.local>
2018-05-14 00:44:51 -0400

    recursion I

--- Recursion/109. Convert Sorted List to Binary Search Tree.cpp
+++ Recursion/109. Convert Sorted List to Binary Search Tree.cpp
@@ -78,3 +78,91 @@ public:
         return root;
     }
 };
+
+
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        auto curr = head;
+        int n = 0;
+        while (curr) {
+            curr = curr->next;
+            ++n;
+        }
+        return BuildBSTFromSortedDoublyListHelper(&head, 0, n);
+    }
+
+    TreeNode * BuildBSTFromSortedDoublyListHelper(ListNode **head, int s, int e) {
+        if (s == e) {
+            return nullptr;
+        }
+
+        int m = s + ((e - s) / 2);
+        auto left = BuildBSTFromSortedDoublyListHelper(head, s, m);
+        auto curr = new TreeNode((*head)->val);
+
+        *head = (*head)->next;
+        curr->left = left;
+        curr->right = BuildBSTFromSortedDoublyListHelper(head, m + 1, e);
+        return curr;
+    }
+};
+
+/*
+Given the sorted linked list: [-10,-3,0,5,9],
+上面解会返回    
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+ 一开始是left[0,2], left[0,1],left[0,0]返回nullptr，
+ 【0，1】创建-10，再right【1,1] 返回nullptr，
+ [0,2] left 返回时-10， 再建立node-3， -10在-3left tree, 再right[2,2] 返回nullptr
+ 形成
+           -3   
+            /   
+          -10 
+
+ 下面解会返回
+       0
+     / \
+   -10   5
+    \     \
+    -3    9
+ 一开始是left[0,1], left[0,-1],返回nullptr，
+ 【0，1】创建-10，再right【1,1] 再建立node-3， -3是-10的right tree, -3的再right[2,1] 返回nullptr
+ 形成
+    -10   
+    \     
+    -3    
+*/
+
+
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        auto curr = head;
+        int n = 0;
+        while (curr) {
+            curr = curr->next;
+            ++n;
+        }
+        return BuildBSTFromSortedDoublyListHelper(&head, 0, n-1);
+    }
+
+    TreeNode * BuildBSTFromSortedDoublyListHelper(ListNode **head, int s, int e) {
+        if (s > e) {
+            return nullptr;
+        }
+
+        int m = s + ((e - s) / 2);
+        auto left = BuildBSTFromSortedDoublyListHelper(head, s, m-1);
+        auto curr = new TreeNode((*head)->val);
+
+        *head = (*head)->next;
+        curr->left = left;
+        curr->right = BuildBSTFromSortedDoublyListHelper(head, m + 1, e);
+        return curr;
+    }
+};
