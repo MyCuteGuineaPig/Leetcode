1a5a29b5d53e5a0c6d66d67aaf8f2aa30d748ad9
beckswu <beckswu@gmail.com>
2018-06-10 23:46:26 -0400

    stack cpp

--- Stack/020. Valid Parentheses.cpp
+++ Stack/020. Valid Parentheses.cpp
@@ -33,3 +33,45 @@ Output: true
 */
 
     
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>stk;
+        unordered_map<char,char>map = {{')','('},{']','['},{'}','{'}};
+        for(auto i: s){
+            if(map.count(i) == 0)
+                stk.push(i);
+            else if(stk.empty() || stk.top()!=map[i])
+                return false;
+            else 
+                stk.pop();
+        }
+        return stk.empty();
+    }
+};
+
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        const  unordered_map<char, char> symbol_pair = {{')', '('},
+                                                        {']', '['},
+                                                        {'}', '{'}};
+        stack<char> parentheses;
+        for (const auto& c: s) {
+            const auto& it = symbol_pair.find(c);
+            if (it != symbol_pair.cend()) {
+                if (parentheses.empty() ||
+                    parentheses.top() != it->second) {
+                    return false;
+                }
+                parentheses.pop();
+            } else {
+                parentheses.emplace(c);
+            }
+        }
+        return parentheses.empty();
+    }
+};
---
+++ Stack/032. Longest Valid Parentheses.cpp
@@ -0,0 +1,19 @@
+/*
+32. Longest Valid Parentheses
+
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+Example 1:
+
+Input: "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()"
+Example 2:
+
+Input: ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()"
+
+
+
+*/
