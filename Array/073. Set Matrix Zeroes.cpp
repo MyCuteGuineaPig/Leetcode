e422d0f6a53cd1165a078e2c5c3950237019eccd
beckswu <beckswu@gmail.com>
2018-08-20 14:43:54 -0400

    Update 073. Set Matrix Zeroes.cpp

--- Array/073. Set Matrix Zeroes.cpp
+++ Array/073. Set Matrix Zeroes.cpp
@@ -74,8 +74,8 @@ public:
             for(int j = 1; j<matrix[0].size(); j++)
                 if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
         
-        if(matrix[0][0] == 0) for(int i = 0; i<matrix[0].size(); i++) matrix[0][i] = 0; //第一列
-        if(col0 == 0) for(int i = 0; i<matrix.size(); i++) matrix[i][0] = 0; //第一行
+        if(matrix[0][0] == 0) for(int i = 0; i<matrix[0].size(); i++) matrix[0][i] = 0; //第一行
+        if(col0 == 0) for(int i = 0; i<matrix.size(); i++) matrix[i][0] = 0; //第一列
         
     }
 };
