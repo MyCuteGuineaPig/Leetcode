c2727adbb67e7fe40ee52b438bfd41e47c281b76
beckswu <beckswu@gmail.com>
2019-03-06 16:42:40 -0500

    Update 113. Path Sum II.cpp

--- DFS/113. Path Sum II.cpp
+++ DFS/113. Path Sum II.cpp
@@ -49,3 +49,35 @@ public:
         cur.pop_back();
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>>  res;
+        vector<int> temp;
+        TreeNode* cur = root, *pre = NULL;
+        stack<TreeNode*>stk;
+        while(cur || ! stk.empty()){
+            if(cur){
+                sum -= cur->val;
+                stk.push(cur);
+                temp.push_back(cur->val);
+                cur = cur->left;
+            }else{
+                cur = stk.top();
+                if(!cur->left && !cur->right && sum == 0)
+                    res.push_back(temp);
+                if(cur->right && pre != cur->right)
+                    cur = cur->right;
+                else{
+                    pre = cur;
+                    sum += cur->val;
+                    cur = NULL;
+                    temp.pop_back();
+                    stk.pop();
+                }
+            }
+        }
+        return res;
+    }
+};
