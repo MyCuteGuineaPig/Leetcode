141063615e8cea173a77c6ebcd724d354621d615
beckswu <beckswu@gmail.com>
2019-03-07 12:07:24 -0500

    Update 236. Lowest Common Ancestor of a Binary Tree.cpp

--- DFS/236. Lowest Common Ancestor of a Binary Tree.cpp
+++ DFS/236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -27,3 +27,34 @@ public:
         return left ? (right ? root : left) : right; 
     }
 };
+
+
+//iterative
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        //iterative, path comparing
+        if(!root || root==p || root==q) return root;
+        vector<TreeNode*> pathp, pathq, temp;
+        temp.push_back(root); //temp是stack，元素是从root到现在node上的路径
+        TreeNode* prev=NULL; //prev是用来返回到原来位置用的
+        while(pathp.empty() || pathq.empty()){
+            root=temp.back();
+            if(root==p) pathp=temp;
+            if(root==q) pathq=temp;
+            if(!root->left && !root->right || !root->right && prev==root->left || root->right && prev==root->right){
+                temp.pop_back();
+                prev=root;
+            }
+            else{
+                if(!root->left || prev==root->left) temp.push_back(root->right);
+                else temp.push_back(root->left);
+            }
+        }
+        int n=min(pathp.size(),pathq.size());
+         for(int i=1; i<n; i++){
+            if(pathp[i]!=pathq[i]) return pathp[i-1];
+        }
+        return pathp[n-1];
+    }
+};
