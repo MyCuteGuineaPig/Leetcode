14f27f9987b4014cfd47560d4b9fb47253c6cc14
beckswu <beckswu@gmail.com>
2019-03-08 10:47:29 -0500

    Update 332. Reconstruct Itinerary.cpp

--- DFS/332. Reconstruct Itinerary.cpp
+++ DFS/332. Reconstruct Itinerary.cpp
@@ -92,3 +92,28 @@ public:
         route.push_back(cur);
     }
 };
+
+
+class Solution {
+        unordered_map<string, priority_queue<string, vector<string>, greater<string>>> graph;
+        vector<string> result;
+        void dfs(string vtex)
+        {
+            auto & edges = graph[vtex];
+            while (!edges.empty())
+            {
+                string to_vtex = edges.top();
+                edges.pop();
+                dfs(to_vtex);
+            }
+            result.push_back(vtex);
+        }
+    public:
+        vector<string> findItinerary(vector<pair<string, string>> tickets) {
+            for (auto e : tickets)
+                graph[e.first].push(e.second);
+            dfs("JFK");
+            reverse(result.begin(), result.end());
+            return result;
+        }
+    };
