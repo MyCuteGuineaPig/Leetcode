a2e14b7dbf0b5fb3a594f9ccfe8de9d42de0febd
beckswu <beckswu@gmail.com>
2019-03-13 11:13:38 -0400

    Update 219. Contains Duplicate II.cpp

--- Hash Table/219. Contains Duplicate II.cpp
+++ Hash Table/219. Contains Duplicate II.cpp
@@ -29,3 +29,19 @@ public:
         return false;
     }
 };
+
+
+
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int>st;
+        for(int i = 0; i<nums.size(); i++){
+            if(i>k) st.erase(nums[i-k-1]);
+            if(st.find(nums[i]) != st.end()) return true;
+            st.insert(nums[i]);
+        }
+        return false;
+    }
+};
