7bd4a4726f65a3b5d0197a833f88994018e48670
beckswu <beckswu@gmail.com>
2019-03-20 11:14:56 -0400

    Update 164. Maximum Gap.py

--- Sort/164. Maximum Gap.py
+++ Sort/164. Maximum Gap.py
@@ -100,4 +100,22 @@ class Solution:
         for i in range(1, len(nums)):
             max_gap = max(max_gap, nums[i]-nums[i-1])
         return max_gap
-        
+
+      
+      
+      
+class Solution:
+# @param num, a list of integer
+# @return an integer
+    def maximumGap(self, num):
+        if len(num) < 2 or min(num) == max(num):
+            return 0
+        a, b = min(num), max(num)
+        size = int(math.ceil((b-a)/(len(num)-1)))
+        bucket = [[None, None] for _ in range((b-a)//size+1)]
+        for n in num:
+            b = bucket[(n-a)//size]
+            b[0] = n if b[0] is None else min(b[0], n)
+            b[1] = n if b[1] is None else max(b[1], n)
+        bucket = [b for b in bucket if b[0] is not None]
+        return max(bucket[i][0]-bucket[i-1][1] for i in range(1, len(bucket)))      
