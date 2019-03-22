9d0b05bbcace193467e664aa12e2b03513451b65
beckswu <beckswu@gmail.com>
2019-03-22 12:14:48 -0400

    Update 95. Unique Binary Search Trees II.py

--- Recursion/95. Unique Binary Search Trees II.py
+++ Recursion/95. Unique Binary Search Trees II.py
@@ -78,3 +78,20 @@ class Solution:
             dp[start][end] = li
             return li
         return [] if n == 0 else generater(1,n)
+      
+      
+      
+      
+      
+def generateTrees(self, n):
+    def node(val, left, right):
+        node = TreeNode(val)
+        node.left = left
+        node.right = right
+        return node
+    def trees(first, last):
+        return [node(root, left, right)
+                for root in range(first, last+1)
+                for left in trees(first, root-1)
+                for right in trees(root+1, last)] or [None]
+    return trees(1, n)
