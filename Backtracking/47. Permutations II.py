3f6488aad0acc2ed9c9c1be5209d51e9863d6906
beckswu <beckswu@gmail.com>
2019-04-10 12:20:42 -0400

    Update 47. Permutations II.py

--- Backtracking/47. Permutations II.py
+++ Backtracking/47. Permutations II.py
@@ -16,6 +16,18 @@ For example,
 """
 
 class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def generate(cur, nums):
+            if not nums:
+                yield cur
+            for i, v in enumerate(nums):
+                if i == 0 or v != nums[i-1]:
+                  for p in generate(cur+[v], nums[:i]+nums[i+1:]):
+                    yield p
+        nums.sort()
+        return [*generate([],nums)]
+
+class Solution:
     def permuteUnique(self, nums):
         ans = [[]]
         for n in nums:
