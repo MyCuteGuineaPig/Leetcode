a98befafaf8a6e13bc11c15ac43ef73ba0d8b2e0
beckswu <beckswu@gmail.com>
2019-04-11 11:07:57 -0400

    Update 003. Longest Substring Without Repeating Characters.py

--- Hash Table/003. Longest Substring Without Repeating Characters.py
+++ Hash Table/003. Longest Substring Without Repeating Characters.py
@@ -10,6 +10,17 @@ Given "bbbbb", the answer is "b", with the length of 1.
 Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 """
 #记录start
+import collections
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        dic = collections.defaultdict(lambda:-1)
+        res, start = 0, -1
+        for i, v in enumerate(s):
+            start = max(start, dic[v])
+            dic[v] = i
+            res = max(res, i - start)
+        return res
+
 class Solution:
     def lengthOfLongestSubstring(self, s):
         """
