a1b884072ace470f3c2626cfcb5a3738365aa7ee
beckswu <beckswu@gmail.com>
2019-04-23 09:42:23 -0400

    Update 845. Longest Mountain in Array.cpp

--- Array/845. Longest Mountain in Array.cpp
+++ Array/845. Longest Mountain in Array.cpp
@@ -79,3 +79,59 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    int longestMountain(vector<int> A) {
+        int N = A.size(), res = 0;
+        vector<int> up(N, 0), down(N, 0);
+        for (int i = N - 2; i >= 0; --i) if (A[i] > A[i + 1]) down[i] = down[i + 1] + 1;
+        for (int i = 0; i < N; ++i) {
+            if (i > 0 && A[i] > A[i - 1]) up[i] = up[i - 1] + 1;
+            if (up[i] && down[i]) res = max(res, up[i] + down[i] + 1);
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int longestMountain(vector<int> A) {
+        int res = 0, up = 0, down = 0;
+        for (int i = 1; i < A.size(); ++i) {
+            if (down && A[i - 1] < A[i] || A[i - 1] == A[i]) up = down = 0;
+            up += A[i - 1] < A[i];
+            down += A[i - 1] > A[i];
+            if (up && down) res = max(res, up + down + 1);
+        }
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    int longestMountain(vector<int>& A) {
+        int ret = 0;
+        int start = -1;
+        int last = -2;
+        for (int i = 0; i < (int)A.size() - 1; i++) {
+            if (A[i] < A[i+1]) {
+                if (last != 1) {
+                    start = i;
+                }
+                last = 1;
+            } else if (A[i] > A[i+1]) {
+                if (start != -1) {
+                    ret = max(ret, i - start + 2);
+                }
+                last = -1;
+            } else {
+                start = -1;
+                last = 0;
+            }
+        }
+        return ret;
+    }
+};
