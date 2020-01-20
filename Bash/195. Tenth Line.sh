6e6978b1deed22705b7eb1accd47a0d50020b86a
beckswu <beckswu@gmail.com>
2020-01-20 14:02:56 -0500

    add new file

--- Bash/195. Tenth Line.sh
+++ Bash/195. Tenth Line.sh
@@ -1,3 +1,20 @@
+# Comment 
+
+# The awk and sed solutions do not exit immediately after printing. 
+# So in case of a large file, they will print the 10th line, but then keep on looping until the end of file.
+
+# For my testcase, I created a file that has 100 million lines. 
+# And then I use the "time" command to time each solution. 
+# Notice that Solutions 1 and 4 are very fast - less than a second, 
+# but solutions 2 and 3 take up anywhere from 12 to 19 seconds, which is an unnecessary time hog.
+
+#The last two awk scripts exit right after printing and you can see that their run times have improved.
+
+#Also, I replaced the "exit 0" in the shell script by a "return",
+# which returns control to the shell prompt but does not close the terminal window.)
+
+
+
 # Solution 1
 cnt=0
 while read line && [ $cnt -le 10 ]; do
@@ -15,6 +32,9 @@ awk 'NR == 10' file.txt
 
 # Solution 3
 sed -n 10p file.txt
+#or
+sed '10!D' file.txt
+
 
 # Solution 4
 tail -n+10 file.txt|head -1 
@@ -22,3 +42,29 @@ tail -n+10 file.txt|head -1
 # Solution 5
 head -10 file.txt | tail -1
 
+
+cut -d$'\n' -f 10 file.txt # $:  $'string' are treated specially. 
+#The word expands to string, with backslash-escaped characters replaced as specified by the ANSI C standard
+
+
+# # ==========  Efficient awk ==========
+time awk 'NR==10{print; exit}' file.txt
+
+#Line 10
+
+#real    0m0.031s
+#user    0m0.000s
+#sys     0m0.030s
+
+time awk 'FNR==10{print; exit}' file.txt
+Line 10
+
+real    0m0.031s
+user    0m0.000s
+sys     0m0.030s
+
+
+#General Solution
+# Extract NLINES lines from STARTING line
+
+STARTING=10; NLINES=1; cat file.txt | tail -n+${STARTING} | head -n${NLINES}
