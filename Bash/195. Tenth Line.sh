ecd847c6ec7dfb8f1ac7c5e3be3ce242e6e1b765
beckswu <beckswu@gmail.com>
2020-01-20 10:58:51 -0500

    add file

---
+++ Bash/195. Tenth Line.sh
@@ -0,0 +1,24 @@
+# Solution 1
+cnt=0
+while read line && [ $cnt -le 10 ]; do
+  let 'cnt = cnt + 1'
+  if [ $cnt -eq 10 ]; then
+    echo $line
+    exit 0
+  fi
+done < file.txt
+
+# Solution 2
+awk 'FNR == 10 {print }'  file.txt
+# OR
+awk 'NR == 10' file.txt
+
+# Solution 3
+sed -n 10p file.txt
+
+# Solution 4
+tail -n+10 file.txt|head -1 
+
+# Solution 5
+head -10 file.txt | tail -1
+
