705bf85dafe07a6aaa5893e980ed6bb0d216990e
beckswu <beckswu@gmail.com>
2020-01-21 11:35:00 -0500

    Update tmp.sh

--- Bash/tmp.sh
+++ Bash/tmp.sh
@@ -62,3 +62,26 @@ do
         echo -n "$word " #不加空格会 接着打 比如 aliceryan 而不是 alice ryan
     fi
 done 
+
+
+#Solution: Using associative array 
+declare -A matrix
+col=1
+row=1
+
+while read line; do
+    col=1
+    for word in $line; do
+            matrix[$row,$col]=$word
+            ((col++))
+    done
+    ((row++))
+done < file.txt
+
+for ((i=1;i<col;i++)) do
+    echo -n ${matrix[1,$i]} 
+    for((j=2;j<row;j++)) do
+            echo -n ' '${matrix[$j,$i]}
+    done
+    echo
+done
