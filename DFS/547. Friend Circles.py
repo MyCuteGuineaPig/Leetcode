348fb37879d53b00af051833294f8e12713f8e01
beckswu <beckswu@gmail.com>
2020-02-01 18:47:24 -0500

    Create 547. Friend Circles.py

---
+++ DFS/547. Friend Circles.py
@@ -0,0 +1,34 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.rank = [0 for i in range(n)]
+        self.size = n
+        self.parents = [i for i in range(n)]
+    
+    def __getitem__(self,x):
+        if self.parents[x] != x:
+            self.parents[x] = self[self.parents[x]]
+        return self.parents[x]
+
+    def union(self,x, y):
+        px, py = self[x], self[y]
+        if px == py:
+            return
+        self.size -= 1
+        if self.rank[px] > self.rank[py]:
+            self.parents[py] = px
+        elif self.rank[px] < self.rank[py]:
+            self.parents[px] = py
+        else:
+            self.parents[py] = px
+            self.rank[px] += 1
+
+class Solution:
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        n = len(M)
+        ds = DisjointSet(n)
+        for  i, row in enumerate(M):
+            for j, value in enumerate(row[i:],i):
+                if value == 1:
+                    ds.union(i,j)
+        
+        return ds.size
