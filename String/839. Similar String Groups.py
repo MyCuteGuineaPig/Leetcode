8f4c67fc4a6bf95f1ed822957ab6a59722d4914b
beckswu <beckswu@gmail.com>
2020-02-02 10:17:45 -0500

    Update and rename 839. Similar String Groups (1).py to 839. Similar String Groups.py

--- String/839. Similar String Groups.py
+++ String/839. Similar String Groups.py
@@ -147,3 +147,37 @@ class Solution:
                     y = x[:i] + x[j] + x[i + 1:j] + x[i] + x[j + 1:]
                     if y in parents: union(x, y)
         return self.count
+
+//快的解
+class Solution:
+    def numSimilarGroups(self, A: List[str]) -> int:
+        m, n = len(A[0]), len(A)
+        A = set(A)
+        p, res = {s : s for s in A}, len(A) # p: parent
+        def find(s):
+            while s != p[s]: 
+                p[s] = p[p[s]]
+                s = p[s]
+            return s
+        def judge(a, b):
+            cnt = 0
+            for i, j in zip(a, b):
+                if i != j:
+                    if cnt < 2: cnt += 1
+                    else: return False
+            return cnt == 2
+        if m > n:
+            print(1)
+            for a, b in itertools.combinations(A, 2): # O(m * n ^ 2)
+                if judge(a, b):
+                    ra, rb = find(a), find(b)
+                    if ra != rb: res, p[ra] = res - 1, rb
+        else: 
+            print(2)
+            for a in A: # O(n * m ^ 2)
+                for i, j in itertools.combinations(range(m), 2):
+                    b = a[:i] + a[j] + a[i + 1: j] + a[i] + a[j + 1:]
+                    if b in A: 
+                        ra, rb = find(a), find(b)
+                        if ra != rb: res, p[ra] = res - 1, rb
+        return res
