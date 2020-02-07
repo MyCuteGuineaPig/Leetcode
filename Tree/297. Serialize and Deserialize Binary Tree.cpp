233dd6941793742c8afb0a86072ce2b28c82f7e4
beckswu <beckswu@gmail.com>
2020-02-07 14:43:16 -0500

    Update 297. Serialize and Deserialize Binary Tree.cpp

--- Tree/297. Serialize and Deserialize Binary Tree.cpp
+++ Tree/297. Serialize and Deserialize Binary Tree.cpp
@@ -164,46 +164,47 @@ private:
 //BFS
 class Codec {
 public:
-
+    
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        queue<TreeNode*> q;
+        ostringstream out;
+        queue<TreeNode*>q;
         q.push(root);
-        string str;
-        while (!q.empty()) {
-            if (q.front() == nullptr) {
-                str = str + "#,";
-            } else {
-                q.push(q.front()->left);
-                q.push(q.front()->right);
-                str = str + to_string(q.front()->val) + ",";
-            }
+        while(!q.empty()){
+            TreeNode* top = q.front();
             q.pop();
+            if(!top) out << "# ";
+            else{
+                out<<top->val<<" ";
+                q.push(top->left);
+                q.push(top->right);
+            }
         }
-        return str;
+        return out.str();
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
         TreeNode* root = nullptr;
-        queue<TreeNode**> q;
+        queue<TreeNode**>q;
         q.push(&root);
-        string::iterator first = data.begin();
-        while (first != data.end()) {
-            TreeNode** pp = q.front();
-            if (*first == '#') {
-                // *pp = nullptr;
-                advance(first, 2);
-            } else {
-                string::iterator last = find(first, data.end(), ',');
-                int val = stoi(string(first, last));
-                *pp = new TreeNode(val);
-                q.push(&((*pp)->left));
-                q.push(&((*pp)->right));
-                first = next(last);
+        istringstream in(data);
+        while(!q.empty()){
+            TreeNode** top = q.front();
+            // TreeNode* top = *(q.front());  *(q.front()) 取值, 然后复制给新的pointer
+            string val;
+            in >> val;
+            cout<<" val "<<val<<endl;
+            if(val == "#")
+                *top = nullptr;
+            else{
+                *top = new TreeNode(stoi(val));
+                q.push(&((*top)->left));
+                q.push(&((*top)->right));
             }
             q.pop();
         }
         return root;
     }
+    
 };
