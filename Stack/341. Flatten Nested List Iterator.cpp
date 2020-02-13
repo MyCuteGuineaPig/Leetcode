89d23263bf07b5c93b7b0f2a427bdddf4db16a40
beckswu <beckswu@gmail.com>
2020-02-12 22:39:00 -0500

    Update 341. Flatten Nested List Iterator.cpp

--- Stack/341. Flatten Nested List Iterator.cpp
+++ Stack/341. Flatten Nested List Iterator.cpp
@@ -253,6 +253,25 @@ public:
  * while (i.hasNext()) cout << i.next();
  */
 
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
 class NestedIterator {
 public:
     NestedIterator(vector<NestedInteger> &nestedList) {
@@ -260,10 +279,9 @@ public:
     }
     
     stack<pair<vector<NestedInteger>::iterator, vector<NestedInteger>::iterator >>stk;
-    int val;
         
     int next() {
-        return val;
+        return (stk.top().first++)->getInteger();
     }
     
     bool hasNext(){
@@ -273,8 +291,6 @@ public:
                 continue;
             }
             if(stk.top().first->isInteger()){
-                val = stk.top().first->getInteger();
-                ++stk.top().first;
                 return true;
             }
             vector<NestedInteger>& newlist = stk.top().first->getList();
@@ -286,6 +302,14 @@ public:
     }
 
 };
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
+
+
 
 
 class NestedIterator {
