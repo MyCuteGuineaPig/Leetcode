4cd2d07c9650391edfb4f9c298fa5c4410d74132
beckswu <beckswu@gmail.com>
2020-02-12 22:26:05 -0500

    Update 341. Flatten Nested List Iterator.cpp

--- Stack/341. Flatten Nested List Iterator.cpp
+++ Stack/341. Flatten Nested List Iterator.cpp
@@ -252,3 +252,38 @@ public:
  * NestedIterator i(nestedList);
  * while (i.hasNext()) cout << i.next();
  */
+
+
+
+class NestedIterator {
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        begins.push(nestedList.begin());
+        ends.push(nestedList.end());
+    }
+
+    int next() {
+        hasNext();
+        return (begins.top()++)->getInteger();
+    }
+
+    bool hasNext() {
+        while (begins.size()) {
+            if (begins.top() == ends.top()) {
+                begins.pop();
+                ends.pop();
+            } else {
+                auto x = begins.top();
+                if (x->isInteger())
+                    return true;
+                begins.top()++;
+                begins.push(x->getList().begin());
+                ends.push(x->getList().end());
+            }
+        }
+        return false;
+    }
+
+private:
+    stack<vector<NestedInteger>::iterator> begins, ends;
+};
