1448d9b8bc826cd79f40c78cb1354603fc2f7209
beckswu <beckswu@gmail.com>
2020-02-14 15:54:13 -0500

    Update 373. Find K Pairs with Smallest Sums.py

--- Heap/373. Find K Pairs with Smallest Sums.py
+++ Heap/373. Find K Pairs with Smallest Sums.py
@@ -95,4 +95,4 @@ class Solution:
     def kSmallestPairs(self, nums1, nums2, k):
         streams = map(lambda u: ([u+v, u, v] for v in nums2), nums1)
         stream = heapq.merge(*streams)
-        return [suv[1:] for suv in itertools.islice(stream, k)]
+        return [suv[1:] for suv in itertools.islice(stream, k)] #
