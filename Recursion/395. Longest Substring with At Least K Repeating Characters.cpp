50efafc4a3e5251790fbcb7125472fcb99dab7c2
beckswu <beckswu@gmail.com>
2020-02-19 15:55:19 -0500

    Update 395. Longest Substring with At Least K Repeating Characters.cpp

--- Recursion/395. Longest Substring with At Least K Repeating Characters.cpp
+++ Recursion/395. Longest Substring with At Least K Repeating Characters.cpp
@@ -72,6 +72,64 @@ public:
 };
 
 
+//Two pointer Solution 
+ /*
+ 
+longestSubstringWithNUniqueChars 当 unique的个数等于target 和大于等于 k的个数等于target 时候更新结果
+
+比如
+    a a  b b a c c d d, k = 2  uniqueTarget = 2
+    0 1  2 3 4 5 6 7 8
+
+  i = 0, unqiueUnqie = 1, numNoLessThanK = 1, begin = 0 
+  i = 1, unqiueUnqie = 1, numNoLessThanK = 1, begin = 0
+  i = 2, unqiueUnqie = 2, numNoLessThanK = 1, begin = 0
+  i = 3, unqiueUnqie = 2, numNoLessThanK = 2, begin = 0, 更新res
+  i = 4, unqiueUnqie = 2, numNoLessThanK = 2, begin = 0, 更新res
+  i = 5, unqiueUnqie = 2, numNoLessThanK = 0, begin = 4
+  i = 6, unqiueUnqie = 2, numNoLessThanK = 1, begin = 4
+  i = 7, unqiueUnqie = 2, numNoLessThanK = 1, begin = 5
+  i = 8, unqiueUnqie = 2, numNoLessThanK = 2, begin = 5, 更新res
+
+  在 index = 3 时候更新结果, res = 4
+  当 index = 4 时候也更新结果 res = 5
+  当 index = 5 时候移动窗口到 begin = 4 , index = 5 前如果有满足的已经更新
+
+ 
+ */
+class Solution {
+public:
+    int longestSubstring(string s, int k) {
+        int res = 0;
+        for(int i = 1; i<=26; i++){
+            res = max(res, longestSubstringWithNUniqueChars());
+        }
+        return res;
+    }
+
+    int longestSubstringWithNUniqueChars(const string& s, const int& k, const int& uniqueTarget){
+        int res = 0;
+        int numUnique = 0; 
+        int numNoLessThanK  = 0;
+        int begin = 0;
+        unordered_map<int,int>cnt;
+        for(int i = 0; i<s.size(); ++i){
+            if(cnt[s[i]] ++ = 0) ++numUnique;
+            if(cnt[s[i]] == k) ++ numNoLessThanK;
+
+            while(numUnique > uniqueTarget){
+                if(cnt[s[begin]]-- == k) --numNoLessThanK;
+                if(cnt[s[begin++]] == 0) --numUnique;
+            }
+
+            if(numUnique == uniqueTarget && numNoLessThanK == uniqueTarget)
+                res = max(res, i-begin+1);
+        
+        }
+        return res;
+    }
+};
+
 
 
 /*
