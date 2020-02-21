826793352e879c4c015692be61ce74c40cf888aa
beckswu <beckswu@gmail.com>
2020-02-21 11:20:59 -0500

    Update 301. Remove Invalid Parentheses.cpp

--- DFS/301. Remove Invalid Parentheses.cpp
+++ DFS/301. Remove Invalid Parentheses.cpp
@@ -181,6 +181,33 @@ public:
 
 /*
 process redundant ")" from left to right, process redundant "(" from right to left
+
+Key Points:
+
+1. Generate unique answer once and only once, do not rely on Set.
+2. Do not need preprocess.
+3. Runtime 3 ms.
+Explanation:
+
+We all know how to check a string of parentheses is valid using a stack. Or even simpler use a counter.
+The counter will increase when it is ‘(‘ and decrease when it is ‘)’. 
+Whenever the counter is negative, we have more ‘)’ than ‘(‘ in the prefix.
+
+To make the prefix valid, we need to remove a ‘)’. 
+The problem is: which one? The answer is any one in the prefix. 
+However, if we remove any one, we will generate duplicate results, for example: s = ()), 
+we can remove s[1] or s[2] but the result is the same (). 
+Thus, we restrict ourself to remove the first ) in a series of concecutive )s.
+
+After the removal, the prefix is then valid. We then call the function recursively to solve the rest of the string. 
+However, we need to keep another information: the last removal position. If we do not have this position, 
+we will generate duplicate by removing two ‘)’ in two steps only with a different order.
+For this, we keep tracking the last removal position and only remove ‘)’ after that.
+
+Now one may ask. What about ‘(‘? What if s = ‘(()(()’ in which we need remove ‘(‘?
+The answer is: do the same from right to left.
+However a cleverer idea is: reverse the string and reuse the code!
+Here is the final implement in Java.
 */
 
 class Solution {
