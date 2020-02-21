ec9082314cfaf56ab06d0d85438fabba68e0c2ee
beckswu <beckswu@gmail.com>
2020-02-20 19:15:46 -0500

    Update 200. Number of Islands.cpp

--- DFS/200. Number of Islands.cpp
+++ DFS/200. Number of Islands.cpp
@@ -23,6 +23,7 @@ Answer: 3
 
 */
 
+//DFS
 class Solution {
 public:
     int n,m;
@@ -126,6 +127,36 @@ public:
     }
 };
 
+
+//BFS
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0, offsets[] = {0, 1, 0, -1, 0};
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    grid[i][j] = '0';
+                    queue<pair<int, int>> todo;
+                    todo.push({i, j});
+                    while (!todo.empty()) {
+                        pair<int, int> p = todo.front();
+                        todo.pop();
+                        for (int k = 0; k < 4; k++) {
+                            int r = p.first + offsets[k], c = p.second + offsets[k + 1];
+                            if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1') {
+                                grid[r][c] = '0';
+                                todo.push({r, c});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return islands;
+    }
+};
 
 //Union Find
 class Solution {
