c6ac31272804d6522303d2973d1659818b06a578
beckswu <beckswu@gmail.com>
2020-02-25 14:09:26 -0500

    Update 40. Combination Sum II.py

--- Backtracking/40. Combination Sum II.py
+++ Backtracking/40. Combination Sum II.py
@@ -1,4 +1,3 @@
-#write by own
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         def generate(cur, target, start):
@@ -11,3 +10,27 @@ class Solution:
                             yield p
         candidates.sort()
         return [*generate([], target, 0)]
+
+# 2020-02-05  
+class Solution:
+    def combinationSum2(self, cd: List[int], target: int, isStart = True) -> List[List[int]]:
+        if isStart:
+            cd.sort()
+        if target == 0:
+            return [[]]
+        if target < 0:
+            return [] 
+        return [[v] + nxt for i, v in enumerate(cd) if i == 0 or cd[i-1] != cd[i] for nxt in self.combinationSum2(cd[i+1:], target - v)]
+
+# 2020-02-05  
+class Solution:
+    def combinationSum2(self, cd: List[int], target: int) -> List[List[int]]:
+        def generate(start, t):
+            if t >= 0:
+                if t == 0: 
+                    yield []
+                for i,v  enumerate(cd[start:], start):
+                    if i == start or v !=  s[i-1]:
+                        for p in generate(i+1, t-v):
+                            yield [i] + p 
+        return [*generate(0,target)]
