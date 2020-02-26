aa1a4f3858524d7747ebe88338aab0c7b036d23e
beckswu <beckswu@gmail.com>
2020-02-25 21:41:40 -0500

    Update 79. Word Search.py

--- Backtracking/79. Word Search.py
+++ Backtracking/79. Word Search.py
@@ -27,3 +27,20 @@ class Solution:
         board[i][j] = ch
         return exist
         
+ 
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        if not board or not(board[0]):
+            return False 
+        def dfs(i, j, word, visited):
+            if not word:
+                return True
+            dir = [0,-1, 0, 1, 0]
+            for x, y in zip(dir[:-1], dir[1:]):
+                x_new, y_new = i + x, j + y 
+                if x_new < 0 or y_new < 0 or x_new >= len(board) or y_new >= len(board[0]) or (x_new,y_new) in visited:
+                    continue 
+                if word[0] == board[x_new][y_new] and dfs(x_new, y_new, word[1:], visited | {(x_new, y_new)}):
+                    return True
+            return False
+        return any(dfs(i, j, word[1:], set([(i,j)])) for i in range(len(board)) for j in range(len(board[0])) if board[i][j] == word[0])
