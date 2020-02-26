9a40375f480154f387555c8445823018fe155a95
beckswu <beckswu@gmail.com>
2020-02-25 21:23:28 -0500

    Update 52. N-Queens II.py

--- Backtracking/52. N-Queens II.py
+++ Backtracking/52. N-Queens II.py
@@ -47,3 +47,45 @@ class Solution:
         self.res = 0
         dfs([],0)
         return self.res
+    
+#2020
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        self.res = 0
+        def dfs(rows, dia1, dia2):
+            row = len(rows)
+            if row == n:
+                self.res += 1
+            if row < n:
+                for j in range (n):
+                    if j not in rows and row + j not in dia1 and row - j not in dia2:
+                        dfs(rows | {j}, dia1 | {row + j}, dia2 | {row - j} )
+                    
+        dfs(set(), set(), set())
+        return self.res
+#2020
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        col = [0 for _ in range(n)]
+        dia1, dia2 = [0 for _ in range(2*n-1)], [0 for _ in range(2*n-1)]
+        self.res = 0
+        def dfs(i):
+            if i == n:
+                self.res += 1
+                return 
+            if i < n:
+                for j in range (n):
+                    if isValid(i, j):
+                        assign(i,j,1)
+                        dfs(i+1)
+                        assign(i, j, 0)
+        
+        def isValid(i, j):
+            return not (col[j] or dia1[n-1+i-j] or dia2[i+j])
+        
+        def assign(i, j, val):
+            col[j] = val
+            dia1[n-1+i-j] = val 
+            dia2[i+j] = val
+        dfs(0)
+        return self.res
