0701d15befb8036d7f55f1964f297a05e2fd7a3d
beckswu <beckswu@gmail.com>
2020-02-27 16:39:44 -0500

    Update 698. Partition to K Equal Sum Subsets.cpp

--- Backtracking/698. Partition to K Equal Sum Subsets.cpp
+++ Backtracking/698. Partition to K Equal Sum Subsets.cpp
@@ -114,3 +114,47 @@ public:
         return false;
     }
 };
+
+
+//DP, slower than DFS 
+//Time Complexity : 0(N*2^N)
+//Space COmplexity: 0(2^N)
+/*
+dp[1] 表示只含有nums[1]能不能组成 分成k份 
+
+dp[5], 101 表示只有 nums[1], nums[3] 能不能分成k 分 
+
+dp[i] indicates whether array of length i can partitioned into k subsets of equal sum. 
+Using this technique, the last index of this dp array
+will tell whether the whole array can be partitioned into k subsets of equal sum.
+
+total[i] stores the sum of subset with sum less than equal to target sum 
+(total sum/k why? because we need to split array into k subset).
+*/
+class Solution {
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        if(nums.empty()) return false;
+        int n = nums.size(), target = 0;
+        vector<bool> dp(1 << n, false); dp[0] = true;  
+        vector<int> sum(1 << n, 0);
+        
+        for(int &x: nums) target += x;
+        if(target % k) return false;
+        target /= k;
+        
+        for(int i = 0; i < (1 << n); ++i){
+            if(dp[i] == false) continue;
+            //int remainder = target - sum[i] % target;
+            for(int j = 0; j < n; ++j){
+                if(i & (1 << j) || nums[j] + sum[i] % target > target) continue; 
+             //i & (1 << j) 比如 binary i = 10101,  1<<j (j=2) binary 100, 表示dp, sum已经包含了j, 
+                int new_state = i | (1 << j);
+                dp[new_state] = true;
+                sum[new_state] = sum[i] + nums[j];
+            }
+        }
+        
+        return dp[(1 << n) - 1];
+    }
+};
