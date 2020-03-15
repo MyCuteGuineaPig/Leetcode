d4e614996e52f2dcd7596d407d53164a820e770a
beckswu <beckswu@gmail.com>
2020-03-14 20:39:05 -0400

    Update 088. Merge Sorted Array.cpp

--- Sort/088. Merge Sorted Array.cpp
+++ Sort/088. Merge Sorted Array.cpp
@@ -59,3 +59,18 @@ public:
         }
     }
 };
+
+
+
+//2020
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i = m+n-1, i1 = m-1, i2 = n -1; i2>=0 ; --i){
+            if(i1 >= 0 && nums1[i1] > nums2[i2])
+                nums1[i] = nums1[i1--];
+            else 
+                nums1[i] = nums2[i2--];
+        }
+    }
+};
