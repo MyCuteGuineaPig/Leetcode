86cb2bac76538455ba2a709b60e17e574eb07e7a
beckswu <beckswu@gmail.com>
2020-03-30 22:23:45 -0400

    add

--- Dynamic Programming/1388. Pizza With 3n Slices.py
+++ Dynamic Programming/1388. Pizza With 3n Slices.py
@@ -35,3 +35,65 @@ class Solution:
                         eat[i%2][j] = max(eat[i%2][j-1], eat[(i-1)%2][j-2] + x)
             return eat[len(arr)%2][-1]
         return max(dp(slices[:-1]), dp(slices[1:]))
+
+"""
+
+Why do we check j - i + 1 < k * 2 - 1,
+instead of something like (k-1) < j - i +1 <= 3 * k
+
+@caohuicn help explain this part:
+Because j - i + 1 is not the number of all the slices that are left.
+You might have chosen not to take a slice in a previous step,
+then there would be leftovers outside of [i, j].
+Now if you take i or j (the start or end slice),
+one of the slices your friends take will be outside of [i, j],
+so the length of [i, j] is reduced by 2, not 3.
+Therefore the minimum number is 2 * k - 1
+(the last step only requires one slice).
+
+"""
+
+class Solution:
+    def maxSizeSlices(self, A):
+        @functools.lru_cache(None)
+        def dp(i, j, k, cycle=0):
+            # i,j = start,end (inclusive)
+            # k = remaining
+            # cycle (see comment on cycle variable)
+            
+            if k == 1:
+                # one remaining, calculate immediately
+                return max(A[i:j + 1])
+            
+            if j - i + 1 < k * 2 - 1:  # j - i + 1 = 5 有5个数, k = 3， 可以选取index 0, 2, 4, 就不会return -float''inf
+                # not possible because not enough elements remain
+                return -float('inf')
+            
+            return max(A[j] +                 # take last element
+                       dp(i + cycle, j - 2,   # dp on i to j-2
+                          k - 1),             # one less element left to take
+                          # (on the cycle variable)
+                          # if the last element of the inital array is taken
+                          # you cannot take the first element of the inital array
+                       # Alternatively, you choose not to take element j
+                       dp(i, j - 1,    # dp on i to j-1
+                          k))          # same number of elements to take
+
+        return dp(0, len(A) - 1, # dp on 0 to length-1
+                  len(A) // 3,   # number of elements to take
+                  1)             # see comment on cycle variable
+            
+        # lru_cache to reduce search space
+
+
+
+class Solution:
+    def maxSizeSlices(self, slices: List[int]) -> int:
+        @functools.lru_cache(None)
+        def dp(i, j, k, firstOne = 0):
+            if k == 1: 
+                return max(slices[i:j+1])
+            if j - i + 1 < 2*k - 1 : 
+                return float('-inf')
+            return max(slices[i] + dp(i+2, j-firstOne, k-1), dp(i+1, j, k))
+        return dp(0, len(slices)-1, len(slices)//3, True)
---
+++ Dynamic Programming/1397. Find All Good Strings.cpp
@@ -0,0 +1,99 @@
+class Solution {
+public:
+    int findGoodStrings(int n, string s1, string s2, string evil) {
+        vector<int> kmp = KMP(evil);
+        vector<vector<vector<vector<long>>>>mp(n+1, vector<vector<vector<long>>>(n+1, vector<vector<long>>(2, vector<long>(2,-1))));
+        return DFS(n, s1, s2, evil, true, true, 0, 0, kmp, mp);
+    }
+    
+    long DFS(const int & n, const  string& s1, const string& s2, const string& evil,  bool useBoundS1, bool useBoundS2, int ind, int ind_evil, const vector<int>& kmp, vector<vector<vector<vector<long>>>>&mp){
+        if( ind_evil == evil.size())
+            return 0;
+        if( ind  == n)
+            return 1;
+        if(mp[ind][ind_evil][useBoundS1][useBoundS2] != -1)
+            return mp[ind][ind_evil][useBoundS1][useBoundS2]; 
+        long tot = 0; 
+        int begin = useBoundS1 ? s1[ind]- 'a' : 0;
+        int end = useBoundS2 ? s2[ind] - 'a' : 'z' - 'a';
+        for(int i = begin ; i< end+1 ; ++i){
+            bool nextUseBoundS1 = useBoundS1 && i == begin;
+            bool nextUseBoundS2 = useBoundS2 && i == end;
+            int j = ind_evil;
+            while ( j && evil[j] - 'a' != i )
+                j = kmp[j -1];
+            if ( i + 'a' == evil[j] )
+                ++j;
+            mp[ind+1][j][nextUseBoundS1][nextUseBoundS2] = DFS(n, s1, s2, evil,nextUseBoundS1 , nextUseBoundS2, ind+1, j, kmp, mp) % (1000000000 + 7);
+            tot =  (tot + mp[ind+1][j][nextUseBoundS1][nextUseBoundS2]) % (1000000000 + 7);
+        }
+        return tot;
+    }
+    
+    vector<int>KMP(const string& str){
+        vector<int>res(str.size());
+        for(int i = 1, j = 0; i<str.size(); ++i){
+            while( j && str[j] != str[i])
+                j = res[j-1];
+            if(str[j] == str[i]) ++j;
+            res[i] = j;
+        }
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    int findGoodStrings(int n, string s1, string s2, string evil) {
+        vector<vector<vector<vector<int>>>> memo = vector(n+1, vector(evil.size()+1, vector(2, vector(2, -1)))); 
+        return dfs(0, 0, true, true,
+			n, s1, s2, evil, computeLPS(evil), memo);
+    }
+    int dfs(int i, int matchEvilLen, bool useBoundS1, bool useBoundS2,
+			int n, string& s1, string& s2, string& evil, const vector<int>& lps, vector<vector<vector<vector<int>>>>& memo) {
+        if (matchEvilLen == evil.size()) return 0; // contain `evil` as a substring -> not good string
+        if (i == n) return 1; // it's a good string
+        if (memo[i][matchEvilLen][useBoundS1][useBoundS2] != -1) return memo[i][matchEvilLen][useBoundS1][useBoundS2];
+        char from = useBoundS1 ? s1[i] : 'a';
+        char to = useBoundS2 ? s2[i] : 'z';
+        int res = 0;
+        for (char c = from; c <= to; c++) {
+            int j = matchEvilLen; // j means nextMatchEvilLen
+            while (j > 0 && evil[j] != c) j = lps[j - 1];
+            if (c == evil[j]) j++;
+            res += dfs(i + 1, j, useBoundS1 && (c == from), useBoundS2 && (c == to),
+                    n, s1, s2, evil, lps, memo);
+            res %= 1000000007;
+        }
+        return memo[i][matchEvilLen][useBoundS1][useBoundS2] = res;
+    }
+    vector<int> computeLPS(const string& str) { // Longest Prefix also Suffix
+        int n = str.size();
+        vector<int> lps = vector<int>(n);
+        for (int i = 1, j = 0; i < n; i++) {
+            while (j > 0 && str[i] != str[j]) j = lps[j - 1];
+            if (str[i] == str[j]) lps[i] = ++j;
+        }
+        return lps;
+    }
+};
+
+
+class Solution {
+public:
+    int maxSizeSlices(vector<int>& slices) {
+        unordered_map<int, unordered_map<int, unordered_map<int, int>>>mp;
+        return dp(0,slices.size()-1, slices.size()/3, slices, mp, 1);
+    }
+    
+    int dp(int i , int j, int k, const vector<int>&slices, unordered_map<int, unordered_map<int, unordered_map<int, int>>>&mp, int lastOne = 0){
+        if(k == 1)
+            return *max_element(slices.begin()+i, slices.begin()+j+1);
+        if(j-i + 1 < 2*k - 1)
+            return numeric_limits<int>::min();
+        if(mp.count(i) && mp[i].count(j) && mp[i][j].count(k))
+            return mp[i][j][k];
+        return mp[i][j][k] = max(slices[j] + dp(i + lastOne, j-2, k-1, slices, mp), dp(i, j-1, k, slices,mp));;
+    }
+};
