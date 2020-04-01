5828a0b2b3e3c6268d3a3125355f156627b4717e
beckswu <beckswu@gmail.com>
2020-04-01 14:44:50 -0400

    Update 091. Decode Ways.py

--- Dynamic Programming/091. Decode Ways.py
+++ Dynamic Programming/091. Decode Ways.py
@@ -13,3 +13,17 @@ class Solution:
             if 10<=int(s[i-1:i+1])<=26:
                 a[i+1] += a[i-1]  
         return a[-1]
+
+"""
+w tells the number of ways
+v tells the previous number of ways
+d is the current digit
+p is the previous digit
+"""
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        v, w, p = 0, int(s>''), ''
+        print(w, s>'')
+        for d in s:
+            v, w, p = w, (d>'0')*w + (9<int(p+d)<27)*v, d
+        return w
