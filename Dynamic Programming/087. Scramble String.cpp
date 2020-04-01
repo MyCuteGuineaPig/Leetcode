988e060111b7020752bd39b61521973912c17564
beckswu <beckswu@gmail.com>
2020-04-01 11:16:52 -0400

    Update 087. Scramble String.cpp

--- Dynamic Programming/087. Scramble String.cpp
+++ Dynamic Programming/087. Scramble String.cpp
@@ -176,3 +176,52 @@ public:
         return DP_helper(s1, s2, 0, 0, sSize, isS);
     }
 };
+
+
+class Solution {
+public:
+    bool isScramble(string s1, string s2) {
+        		/**
+		 * Let F(i, j, k) = whether the substring S1[i..i + k - 1] is a scramble of S2[j..j + k - 1] or not
+		 * Since each of these substrings is a potential node in the tree, we need to check for all possible cuts.
+		 * Let q be the length of a cut (hence, q < k), then we are in the following situation:
+		 * 
+		 * S1 [   x1    |         x2         ]
+		 *    i         i + q                i + k - 1
+		 * 
+		 * here we have two possibilities:
+		 *      
+		 * S2 [   y1    |         y2         ]
+		 *    j         j + q                j + k - 1
+		 *    
+		 * or 
+		 * 
+		 * S2 [       y1        |     y2     ]
+		 *    j                 j + k - q    j + k - 1
+		 * 
+		 * which in terms of F means:
+		 * 
+		 * F(i, j, k) = for some 1 <= q < k we have:
+		 *  (F(i, j, q) AND F(i + q, j + q, k - q)) OR (F(i, j + k - q, q) AND F(i + q, j, k - q))
+		 *  
+		 * Base case is k = 1, where we simply need to check for S1[i] and S2[j] to be equal 
+		 * */
+        
+        if(s1.size() != s2.size())
+            return false;
+        int n = s1.size();
+        vector<vector<vector<int>>>F(n, vector<vector<int>>(n, vector<int>(n+1)));
+        for(int k = 1; k<=n; ++k)
+            for(int i = 0; i + k <= n; ++i)
+                for(int j = 0; j + k <= n; ++j){
+                    if(k == 1)
+                        F[i][j][k] = s1[i] == s2[j];
+                    else{
+                        for(int q = 1; q<k && !F[i][j][k]; ++q){
+                            F[i][j][k] = F[i][j][q] && F[i+q][j+q][k-q] || F[i][j+k-q][q] && F[i+q][j][k-q]; 
+                        }
+                    }
+                }
+        return F[0][0][n];
+    }
+};
