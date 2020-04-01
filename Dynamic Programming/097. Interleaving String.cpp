39aefa8e9d2b4a529a7231aba5da8c2f504b50fc
beckswu <beckswu@gmail.com>
2020-04-01 18:36:04 -0400

    Update 097. Interleaving String.cpp

--- Dynamic Programming/097. Interleaving String.cpp
+++ Dynamic Programming/097. Interleaving String.cpp
@@ -90,3 +90,32 @@ public:
         return false;
     }
 };
+
+//BFS
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if(s1.size() + s2.size() != s3.size())
+            return false;
+        queue<pair<int,int>>q;
+        unordered_set<int>visited;
+        q.push({0,0});
+        while(!q.empty())
+        {
+            auto [i, j] = q.front(); q.pop();
+            if(i + j == s3.size())
+                return true;
+            if(visited.count(i*s3.size() + j))
+                continue;
+            else{
+                visited.insert(i*s3.size()+j);
+                if(i < s1.size() && s1[i] == s3[i+j])
+                    q.push({i+1, j});
+                if(j < s2.size() && s2[j] == s3[i+j])
+                    q.push({i, j+1});
+                
+            }
+        }
+        return false;
+    }
+};
