29eb80d315b5ead223492ef9f6308bfba642c8e8
beckswu <beckswu@gmail.com>
2020-07-13 10:43:44 -0500

    add

--- Dynamic Programming/664. Strange Printer.cpp
+++ Dynamic Programming/664. Strange Printer.cpp
@@ -1,24 +1,4 @@
 /*
-664. Strange Printer
-There is a strange printer with the following two special requirements:
-
-The printer can only print a sequence of the same character each time.
-At each turn, the printer can print new characters starting from and ending at any places, and will cover the original existing characters.
-Given a string consists of lower English letters only, your job is to count the minimum number of turns the printer needed in order to print it.
-
-Example 1:
-Input: "aaabbb"
-Output: 2
-Explanation: Print "aaa" first and then print "bbb".
-Example 2:
-Input: "aba"
-Output: 2
-Explanation: Print "aaa" first and then print "b" from the second place of the string, which will cover the existing character 'a'.
-Hint: Length of the given string will not exceed 100.
-
-*/
-
-/*
 
         a  b  c  a  b  c
 
@@ -42,6 +22,37 @@ Hint: Length of the given string will not exceed 100.
 
 
 */
+
+/* 
+
+"ddaacca"
+*/
+
+//2020
+class Solution {
+public:
+    int strangePrinter(string s) {
+        int n = s.size();
+        if(n == 0)
+            return 0;
+        vector<vector<int>>dp(n, vector<int>(n, n+1));
+        for(int r = 0; r< n; ++r){
+            dp[r][r] = 1;
+            for(int l = r-1; l>=0; --l){
+                if(s[l] == s[r])
+                    dp[l][r] = min(dp[l+1][r], dp[l][r-1]);
+                else{
+                    for(int k = l; k<r; ++k)
+                        dp[l][r] = min(dp[l][r], dp[l][k] + dp[k+1][r]);
+                }
+            }
+        }
+
+        return dp[0][n-1];
+    }
+};
+
+
 
 class Solution {
 public:
@@ -94,29 +105,7 @@ public:
 };
 
 
-/*
 
-
-        a  b  c  a  b  c
-
-    a   1  2  3  3  4  5
-    b      1  2  3  3  4 
-    c         1  2  3  3 
-    a            1  2  3 
-    b               1  2
-    c                  1
-
-
-        a  a  a  b  b  b
-
-    a   1  1  1  2     
-    a      1  1  2      
-    a         1  2     1 
-    b            1  1  1 
-    b               1  1
-    b                  1
-
-*/
 
 
 class Solution {
