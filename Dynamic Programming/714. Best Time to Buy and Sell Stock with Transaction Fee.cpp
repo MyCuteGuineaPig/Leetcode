bb9baa6cbbb99478ade31db30665cfcb4fa7967a
beckswu <beckswu@gmail.com>
2020-07-15 15:06:36 -0500

    add

--- Dynamic Programming/714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
+++ Dynamic Programming/714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -1,33 +1,3 @@
-/*
-714. Best Time to Buy and Sell Stock with Transaction Fee
-
-Your are given an array of integers prices, for which the i-th element is the price of a given stock on day i; 
-and a non-negative integer fee representing a transaction fee.
-
-You may complete as many transactions as you like, but you need to pay the transaction fee for each transaction. 
-You may not buy more than 1 share of a stock at a time (ie. you must sell the stock share before you buy again.)
-
-Return the maximum profit you can make.
-
-Example 1:
-Input: prices = [1, 3, 2, 8, 4, 9], fee = 2
-Output: 8
-Explanation: The maximum profit can be achieved by:
-Buying at prices[0] = 1
-Selling at prices[3] = 8
-Buying at prices[4] = 4
-Selling at prices[5] = 9
-The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
-Note:
-
-0 < prices.length <= 50000.
-0 < prices[i] < 50000.
-0 <= fee < 50000.
-
-
-
-*/
-
 
 
 /*
@@ -71,8 +41,24 @@ public:
         int buy = INT_MAX, sell = 0;
         for(int i = 0; i<prices.size();i++){
             buy = min(buy, prices[i]-sell+fee); //buy 和sell 只会变化一个
-            sell = max(sell, prices[i]-buy);
+            sell = max(sell, prices[i]-buy);// buy 更新因为遇到低的价格, 低的价格不会卖
         }
         return sell;
+    }
+};
+
+class Solution {
+public:
+    int maxProfit(vector<int>& p, int fee) {
+        int n = p.size();
+        if (n < 2) return 0;
+        vector<int> buy(n, 0), sell(n, 0);
+        buy[0] = -p[0];
+        for (int i = 1; i < n; i++) {
+            buy[i] = max(buy[i - 1], sell[i - 1] - p[i]);
+            sell[i] = max(sell[i - 1], buy[i - 1] - fee + p[i]);
+        }
+
+        return sell[n - 1];
     }
 };
--- Dynamic Programming/714. Best Time to Buy and Sell Stock with Transaction Fee.py
+++ Dynamic Programming/714. Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -13,3 +13,17 @@ class Solution:
             buy = min(buy, i-sell+fee)
             sell = max(sell, i-buy)
         return sell
+
+class Solution(object):
+    def maxProfit(self, prices, fee):
+        state = profit = 0 
+        last_price = prices[0] 
+        for price in prices[1:]:                  
+            state += price - last_price
+            if state > fee:
+                profit += state - fee
+                state = fee
+            else:
+                if state < 0: state = 0 #当遇到低的价格时候
+            last_price = price
+        return profit
