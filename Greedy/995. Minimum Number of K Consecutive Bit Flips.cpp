d763c6dbbf784bfa3caee2b529f23a098be4fb5c
beckswu <beckswu@gmail.com>
2020-09-05 21:34:10 -0500

    add

--- Greedy/995. Minimum Number of K Consecutive Bit Flips.cpp
+++ Greedy/995. Minimum Number of K Consecutive Bit Flips.cpp
@@ -1,3 +1,9 @@
+/**
+ * 2020 
+ 如果现在这位开头是0, 必须作为flip的起点, ++cnt, 如果cnt是偶数，cur = A[i]不变，
+                                            如果cnt是奇数, cur = A[i]^1
+ */
+
 class Solution {
 public:
     int minKBitFlips(vector<int>& A, int K) {
