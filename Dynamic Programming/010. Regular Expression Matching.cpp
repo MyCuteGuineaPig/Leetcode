c8a62925f2dc001d751faf63b1a55e47b43f774a
beckswu <beckswu@gmail.com>
2021-04-11 23:21:08 -0500

    Update 010. Regular Expression Matching.cpp

--- Dynamic Programming/010. Regular Expression Matching.cpp
+++ Dynamic Programming/010. Regular Expression Matching.cpp
@@ -25,24 +25,55 @@ isMatch("aab", "c*a*b") → true
 
 */
 
-//2020
+/*
+* could for example be expanded to `` (zero characters), . (one character), .. meaning two characters, or ... (three chars), .... (four chars) etc.. 
+and the .. matches ab. .* just sort of means "insert anything". or nothing. nothing also technically matches.
+
+1. * remove 前面的character
+dp[i+1][j+1] = dp[i+1][j-1] Match zero time preceding. e.g. s = "ab" t = "abb*"
+
+2. * repeat 前面character 0 次
+   
+   t =  a  b  *  c
+s=  1   0  0  0  0
+ a  0   1  0  1  0 
+ b  0   0  1 _1_ =====>  i = 1, j = 2   dp[i+1][j+1] <= take dp[i][j+1],  
+ c  0   0  0  0  1        take  dp[i][j+1] 原因是, dp[i][j+1] = dp[1][3] 只考虑了 s[i-1] ('a') 和 p[j-2] ('a')  match 不 match
+                            s 和 t 再多考虑一位,  if s[i]  == p[j-1]  s= 'ab', t='ab'  dp[i+1][j+1] = dp[i][j+1] = true;
+
+                          
+                          
+3. * repeat 前面多个 character 
+
+
+   t =  a  b  *  c
+s=  1   0  0  0  0 
+ a  0   1  0  1  0 
+ b  0   0  1  1 ===== > dp[i+1][j]: 表示 * 重复了0遍或者多变
+ b  0   0  0 _1_ =====>  i = 2, j = 2   dp[i+1][j+1] <= take dp[i][j+1], 
+ c  0   0  0  0  1        take  dp[i][j+1] 原因是, dp[i][j+1] = dp[2][3] 只考虑了 s[i-1] ('ab') 和 p[j-2] ('ab')  match 不 match, 
+                         如果 s再多考虑一位, 因为是*, t是跟之前一样的pattern,  if s[i]  == p[j-1]  s= 'abb', t='ab'  dp[i+1][j+1] = dp[i][j+1] = true;
+*/
+
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        vector<vector<int>>dp(p.size()+1, vector<int>(s.size()+1,0));
-        int sz = s.size(), pz = p.size();
-        dp[0][0] = 1;
-        for(int i = 0; i<p.size(); ++i)
-            if(p[i] == '*' ) dp[i+1][0] = dp[i-1][0];
-        for(int i = 0; i<pz; ++i){
-            for(int j = 0; j<sz; ++j){
-                if (s[j] == p[i] || p[i] == '.' )
-                    dp[i+1][j+1] = dp[i][j];
-                else if(p[i] == '*') 
-                    dp[i+1][j+1] = dp[i-1][j+1] || (s[j] == p[i-1] || p[i-1] == '.') && dp[i+1][j];
-            }
-        }
-        return dp[pz][sz];
+       int n1 = s.size(), n2 = p.size();
+       vector<vector<int>>dp(n1+1, vector<int>(n2+1, 0));
+       dp[0][0] = 1;
+       for(int j = 1; j<n2 && p[j] == '*'; j+=2)
+           dp[0][j+1] = dp[0][j-1];
+        
+       for(int i = 0; i<n1; ++i)
+       {
+           for(int j = 0; j<n2; ++j){
+               if(s[i] == p[j] || p[j] == '.')
+                   dp[i+1][j+1] = dp[i][j];
+               else if(p[j] == '*')
+                   dp[i+1][j+1] = dp[i+1][j-1] || ( s[i] == p[j-1] || p[j-1] == '.') && dp[i][j+1];
+           }
+       }
+        return dp[n1][n2];
     }
 };
 
