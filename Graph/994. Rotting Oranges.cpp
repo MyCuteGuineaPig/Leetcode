4b2d46044dad25a0808a665be520e1469f7b4c45
beckswu <beckswu@gmail.com>
2024-03-03 19:25:51 -0500

    graph

--- Graph/994. Rotting Oranges.cpp
+++ Graph/994. Rotting Oranges.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>>dir = {{-1, 0}, {0, -1}, {1,0}, {0,1}};
-    
+
     void dfs(vector<vector<int>>& grid, int i, int j, vector<vector<int>>&dp, int level){
         if (dp[i][j] >0 && dp[i][j]<=level) return;
         dp[i][j] = level;
@@ -38,5 +38,71 @@ public:
             }
         }
         return max_cnt;
+    }
+};
+
+
+
+//BFS 注意*m 不是乘以n
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<int>rotten;
+        unordered_set<int>fresh;
+        vector<vector<int>>dir = {{-1, 0}, {0, -1}, {1,0}, {0,1}};
+        int n = grid.size(), m =grid[0].size();
+        for(int i = 0; i<n; ++i){
+            for(int j = 0; j<m; ++j){
+                if(grid[i][j] == 2) {
+                    rotten.push(i*m + j);
+                }
+                if(grid[i][j] == 1) {
+                    fresh.insert(i*m + j);
+                }
+            }
+        }
+        int cnt = 0;
+        while(!rotten.empty() && !fresh.empty()){//需要fresh 不是empty 
+        //如果只是!rotten.empty(), 结果需要cnt - 1
+            queue<int>next;
+            ++cnt; 
+            int size = rotten.size();
+            for(int i = 0; i<size; ++i){
+                int top = rotten.front();  rotten.pop();
+                int r_x = top / m, r_y = top % m;
+                for(auto d: dir){
+                    int x = r_x + d[0], y = r_y + d[1];
+                    if(x >= 0 && y >= 0 && x<n && y < m && fresh.count(x*m + y)){
+                        fresh.erase(x*m + y);
+                        next.push(x*m + y);
+                    }
+                }
+            }
+            rotten.swap(next);
+        }
+        return fresh.empty() ? cnt: -1;
+    }
+};
+
+
+class Solution {
+public:
+    int rot(vector<vector<int>>& g, int i, int j, int d) {
+        if (i < 0 || j < 0 || i >= g.size() || j >= g[i].size() || g[i][j] != 1) return 0;
+        g[i][j] = d + 3;
+        return 1;
+    }
+    int orangesRotting(vector<vector<int>>& g, int d = 0, int fresh = 0) {
+        for (auto i = 0; i < g.size(); ++i) 
+            fresh += count_if(begin(g[i]), end(g[i]), [](int j) { return j == 1; });
+        for (auto old_fresh = fresh; fresh > 0; ++d) {
+            for (auto i = 0; i < g.size(); ++i)
+            for (auto j = 0; j < g[i].size(); ++j)
+                if (g[i][j] == d + 2)
+                fresh -= rot(g,i+1,j,d) + rot(g,i-1,j,d) + rot(g,i,j+1,d) + rot(g,i,j-1,d);
+            if (fresh == exchange(old_fresh, fresh)) return -1;
+        }
+        return d;
     }
 };
--- Graph/994. Rotting Oranges.py
+++ Graph/994. Rotting Oranges.py
@@ -10,3 +10,49 @@ class Solution:
             fresh -= rotting
             timer += 1
         return timer
+
+
+class Solution:
+    def orangesRotting(self, grid):
+        m, n, queue, fresh = len(grid), len(grid[0]), deque(), 0
+        for i,j in product(range(m), range(n)):
+            if grid[i][j] == 2: queue.append((i,j))
+            if grid[i][j] == 1: fresh += 1
+        dirs = [[1,0],[-1,0],[0,1],[0,-1]]
+        levels = 0
+        
+        while queue:
+            levels += 1
+            for _ in range(len(queue)):
+                x, y = queue.popleft()
+                for dx, dy in dirs:
+                    if 0<=x+dx<m and 0<=y+dy<n and grid[x+dx][y+dy] == 1:
+                        fresh -= 1
+                        grid[x+dx][y+dy] = 2
+                        queue.append((x+dx, y+dy))
+                        
+        return -1 if fresh != 0 else max(levels-1, 0)
+
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        visit, curr = set(), deque()
+		# find all fresh and rotten oranges
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    visit.add((i, j))
+                elif grid[i][j] == 2:
+                    curr.append((i, j))
+        result = 0
+        while visit and curr:
+			# BFS iteration
+            for _ in range(len(curr)):
+                i, j = curr.popleft()  # obtain recent rotten orange
+                for coord in ((i-1, j), (i+1, j), (i, j-1), (i, j+1)):
+                    if coord in visit:  # check if adjacent orange is fresh
+                        visit.remove(coord)
+                        curr.append(coord)
+            result += 1
+		# check if fresh oranges remain and return accordingly
+        return -1 if visit else result
