38ed48b0e746d6d0426b884cffa3daa4283c77f0
Macha <jwm05077@gmail.com>
2025-10-14 19:55:38 -0400

    update

--- Dynamic Programming/546. Remove Boxes.cpp
+++ Dynamic Programming/546. Remove Boxes.cpp
@@ -214,3 +214,30 @@ public:
     return f(0, n-1, 0);
     }
 };
+
+
+//2025 Bottom-up
+class Solution {
+public:
+    int removeBoxes(vector<int>& boxes) {
+        int n = boxes.size();
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(n, vector<int>(n,0)));
+        for(int i = 0; i < n; ++i){
+            for(int k = 0; k < n-i; ++k)
+                dp[i][i][k] = (k+1)*(k+1);
+        }
+
+        for(int i = n-2; i >=0; --i){
+            for(int j = i + 1; j < n; ++j){
+                for(int k = 0; k <n-j; ++k){
+                    dp[i][j][k] = dp[i][j-1][0] + (k+1)*(k+1);
+                    for(int m = i; m < j ; ++m){
+                        if (boxes[m] == boxes[j])
+                            dp[i][j][k] = max(dp[i][j][k], dp[i][m][k+1] + dp[m+1][j-1][0]);
+                    }
+                }
+            }
+        }
+        return dp[0][n-1][0];
+    }
+};
